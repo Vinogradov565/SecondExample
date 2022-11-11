8ff354d816ab6ef9b339063b6f4cd94b0b6144fa
Vinogradov565 <116278453+Vinogradov565@users.noreply.github.com>
2022-11-11 15:59:42 +0300

    1.0

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int SanyaDick = 1;
+        int SanyaSmart = 0;
+        boolean b = SanyaDick > SanyaSmart;
+        SanyaSmart = 2;
+        int sanyaDick = SanyaDick;
 
     }
 }
