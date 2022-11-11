ebbbf1b163fb6ce45c761eb735ff195ef042f4c8
Vinogradov565 <116278453+Vinogradov565@users.noreply.github.com>
2022-11-11 17:15:29 +0300

    Создан класс Car Добавлен конструктор Машины Добавлены Аварии

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,23 @@
 public class Car {
+    public String model;
+    public int age;
+    private int accident = 1;
+    private boolean accidents;
+
+     Car() {
+        this.accident = accident;
+    }
+    int getAccident(){
+        return accident;
+    }
+    void start() {
+        accidents = true;
+        System.out.println("У машины есть Аварии");
+    }
+    void stop() {
+        accidents = false;
+        System.out.println("У машины нет Аварий");
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int SanyaDick = 1;
-
+        Car audi = new Car();
+        audi.model = "AudiR8";
+        audi.age = 5;
+        Car myCar = new Car();
+        System.out.println("Model:"   + audi.model);
+        System.out.println("Машине лет:"  + audi.age+"лет");
+        System.out.println("Аварийу машины:"+myCar.getAccident());
+        myCar.start();
     }
 }
